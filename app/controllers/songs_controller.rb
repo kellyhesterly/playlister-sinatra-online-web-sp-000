62e63580a0d761fba1cd6cbb4e7ae24b0316e016
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:37:14 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,25 +13,25 @@ class SongsController < ApplicationController
   get '/songs/new' do
     erb :'/songs/new'
   end
-  #
+
   post '/songs' do
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
 
-    # binding.pry
     flash[:message] = "Successfully created song."
     redirect to("/songs/#{@song.slug}")
   end
-  #
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
-  #
+
   get 'songs/:slug/edit' do
-  
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
   end
 
 end
