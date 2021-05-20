71fa76c2ab69400d5d71c64ec48036982c99cf92
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:52:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
-  get 'songs/:slug/edit' do
+  get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/edit'
   end
