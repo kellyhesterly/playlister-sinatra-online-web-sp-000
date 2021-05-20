fd90dda91318c2e8bcc5f7e1820a182d61ac03b3
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:49:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,7 +35,13 @@ class SongsController < ApplicationController
   end
 
   patch '/songs/:slug' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
 
+    flash[:message] = "Successfully updated song."
+    redirect to("/songs/#{@song.slug}")
   end
 
 end
