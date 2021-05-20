4ee1ea3b4dde70f8244e3aab452e9a27863c9ad5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:28:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,9 +18,8 @@ class SongsController < ApplicationController
     @song = Song.create(params[:song])
     @song.genre_ids = params[:genres]
     @song.save
-    if !params["artist"]["name"].empty?
-      @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    end
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+
     # binding.pry
     flash[:message] = "Successfully created song."
     redirect to("/songs/#{@song.slug}")
