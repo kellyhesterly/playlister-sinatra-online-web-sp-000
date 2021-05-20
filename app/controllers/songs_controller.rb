eb18c8cb623abbbc8a5e21a7c570ba340295a3d6
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:53:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -31,7 +31,7 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/edit'
+    erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
@@ -42,7 +42,7 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully updated song."
-    redirect to("/songs/#{@song.slug}")
+    redirect("/songs/#{@song.slug}")
   end
 
 end
