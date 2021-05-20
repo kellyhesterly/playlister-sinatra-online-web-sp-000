f9e0b5e558521fd38415abccae8f1cd5e761a381
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:54:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,6 @@ require 'sinatra/base'
 require 'rack-flash'
 
 class SongsController < ApplicationController
-  enable :sessions
   use Rack::Flash
 
   get '/songs' do
@@ -14,6 +13,12 @@ class SongsController < ApplicationController
     erb :'/songs/new'
   end
 
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/show'
+  end
+
   post '/songs' do
     @song = Song.create(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
@@ -21,17 +26,12 @@ class SongsController < ApplicationController
     @song.save
 
     flash[:message] = "Successfully created song."
-    redirect to("/songs/#{@song.slug}")
-  end
-
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
+    redirect("/songs/#{@song.slug}")
   end
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/edit'
+    erb :'songs/edit'
   end
 
   patch '/songs/:slug' do
