2cbbd3d5a7e521890a985203601cfd521b423770
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-05-20 22:18:46 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,36 @@
+require 'sinatra/base'
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+  #
+  post '/songs' do
+    @song = Song.create(params[:song])
+    @song.artist = Artist.create(name: params[:artist][:name])
+    @song.genre_ids = params[:genres]
+    @song.save
+    # binding.pry
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
+  end
+  #
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+  #
+  # get 'songs/:slug/edit' do
+  #
+  # end
+
+end
